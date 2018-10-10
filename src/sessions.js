5f2fbfad7e9610a118543271b0fa415cf7df78c7
Carl Schmidt <carl.schmidt@gmail.com>
2018-10-10 22:53:17 +0000

    Placeholder sessions API endpoint

--- src/sessions.js
+++ src/sessions.js
@@ -1,10 +1,16 @@
 'use strict'
 const serverless = require('serverless-http')
 const express = require('express')
+const uuid = require('uuid/v1')
 const sessions = express()
 
 sessions.post('*', function(req, res) {
-  res.send('sessions!')
+  let session = {
+    session_id: uuid(),
+    queue_url: '[queue_url]'
+  }
+  res.json(session)
+  console.log('req', req)
 })
 
 module.exports.handler = serverless(sessions)
