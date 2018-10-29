71c492112d987e5fabea465657417a8e35b46424
Carl Schmidt <carl.schmidt@gmail.com>
2018-10-29 21:50:52 +0000

    Try without serverless-http

--- src/sample-function.js
+++ src/sample-function.js
@@ -1,17 +1,20 @@
 'use strict'
-const serverless = require('serverless-http')
-const express = require('express')
-const sample = express()
 
-
-sample.get('*', (req, res) => {
+module.exports.handler = async function(event, context) {
+  console.log(event)
+  let response = {
+    statusCode: 200,
+    headers: {
+      'x-custom-header': 'My Header Value'
+    }
+  }
   if (!process.env.RESOURCE_PREFIX) {
-    res.status(500, 'Service not configured')
+    response.statusCode = 500
+    response.body = 'Service not configured'
     console.log('Must set RESOURCE_PREFIX environment variable to the S3 bucket name')
   }
   else {
-    res.json({ bucket: process.env.RESOURCE_PREFIX })
+    response.body = JSON.stringify({ bucket: process.env.RESOURCE_PREFIX })
   }
-})
-
-module.exports.handler = serverless(sample)
+  return response
+}
