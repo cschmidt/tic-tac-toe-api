673460cf3c032b105cb68b3e3bd8a392fe46d647
Carl Schmidt <carl.schmidt@gmail.com>
2018-10-29 23:39:26 +0000

    Now validates presence of session_id in command

--- src/command-receiver.js
+++ src/command-receiver.js
@@ -3,9 +3,32 @@ const serverless = require('serverless-http')
 const express = require('express')
 const commandReceiver = express()
 const sns = require('aws-sdk/clients/sns')
+const s3 = require('aws-sdk/clients/s3')
 const bodyParser = require('body-parser')
 
-function submitCommand(command) {
+async function validateCommand(command) {
+  // make sure there's a session id
+  if (!command.session_id) {
+    throw new Error(`Missing session_id in "${command}"`)
+  }
+  else {
+    let session_meta = await fetchSessionMeta(command.session_id)
+  }
+  // ultimately, ensure it conforms to a valid command
+  return command
+}
+
+async function fetchSessionMeta(session_id) {
+  let s3Client = new s3()
+  let sessionFileParams = {
+    Bucket: process.env.RESOURCE_PREFIX,
+    Key: `sessions/${session_id}`
+  }
+  return s3Client.headObject(sessionFileParams).promise()
+}
+
+
+async function submitCommand(command) {
   let snsClient = new sns()
   let messageParams = {
     Message: JSON.stringify(command),
@@ -16,14 +39,16 @@ function submitCommand(command) {
 
 commandReceiver.use(bodyParser.json())
 
-commandReceiver.post('*', function(req, res) {
-  // FIXME: validate the command syntax, check session validity
-  submitCommand(req.body).then(snsResponse => {
+commandReceiver.post('*', async function(req, res) {
+  try {
+    let command = await validateCommand(req.body)
+    let snsResponse = await submitCommand(command)
     res.json(snsResponse)
-  }).catch(err => {
+  }
+  catch (err) {
     res.status(500).send('Error')
     console.log('req', req, 'err', err)
-  })
+  }
 })
 
 module.exports.handler = serverless(commandReceiver)
