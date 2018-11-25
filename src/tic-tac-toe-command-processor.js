15f0c4fa6369e5c44f12b08e1e70746de335b8c4
Carl Schmidt <carl.schmidt@gmail.com>
2018-11-25 07:20:48 +0000

    Now properly parses incoming commands

--- src/tic-tac-toe-command-processor.js
+++ src/tic-tac-toe-command-processor.js
@@ -2,15 +2,18 @@
 const { CommandProcessor } = require('./command-processor')
 const { start_game } = require('./tic-tac-toe-commands')
 
-let commandProcessor = new CommandProcessor()
-commandProcessor.addCommandHandler('start_game', start_game)
-
 
 module.exports.handler = async function(event, context) {
-  console.log('event', event, 'context', context)
+  let commandProcessor = new CommandProcessor()
+  commandProcessor.addCommandHandler('start_game', start_game)
   let commands = []
   for (let record of event.Records) {
-    commands.push(JSON.parse(record.body).Message)
-    commandProcessor.handle(commands)
+    let parsedRecord = JSON.parse(record.body)
+    let parsedMessage = JSON.parse(parsedRecord.Message)
+    for (let command of parsedMessage.commands) {
+      commands.push(command)
+    }
   }
+  console.log('commands', commands)
+  return commandProcessor.handle(commands)
 }
